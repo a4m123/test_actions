c37ec66ce80d0585ce78920113522427bbafd792
a4m123 <quasaq69@gmail.com>
2024-07-30 11:56:12 +0700

    123

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
     std::cout << "How are you?" << std::endl;
-    int a = 0;
+    int a = 123;
     std::cout << a;
     return 0;
 }
